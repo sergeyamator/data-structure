ae86adc7a0ab2bfcc3e2a0f91ea3c37b5e0fc2dd
sergey <sramator@gmail.com>
2018-12-12 14:42:19 +0200

    Add queues

---
+++ queues.js
@@ -0,0 +1,35 @@
+// Queues
+
+function createQueue() {
+  const queue = [];
+
+  return {
+    add(item) {
+      queue.unshift(item);
+    },
+    remove() {
+      queue.pop();
+    },
+    next() {
+      return queue[queue.length - 1];
+    },
+    getLength() {
+      return queue.length;
+    },
+    isEmpty() {
+      return queue.length === 0;
+    }
+  };
+}
+
+const queue = createQueue();
+console.log(queue.isEmpty());
+
+queue.add('First item');
+queue.add('Second item');
+queue.add('Third item');
+
+console.log(queue.next());
+
+queue.remove();
+console.log(queue.next());
